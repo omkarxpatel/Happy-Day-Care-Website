c3bfd7ae903edb95169885065077d53d941cb2ca
omkarxpatel <bendonveryt@gmail.com>
2023-03-19 14:31:46 -0700

    fda

--- source/script.js
+++ source/script.js
@@ -3,31 +3,31 @@ const text = ["Happy Day Care", "Tailored Care for your Child", "Flexible Hours"
 let b = 0;
 
 function type() {
-  let i = 0;
-  const intervalId = setInterval(() => {
-    title.textContent += text[b].charAt(i);
-    i++;
-    if (i >= text[b].length) {
-        b = (b + 1) % text.length;
+    let i = 0;
+    const intervalId = setInterval(() => {
+        title.textContent += text[b].charAt(i);
+        i++;
+        if (i >= text[b].length) {
+            b = (b + 1) % text.length;
 
-        clearInterval(intervalId);
-        setTimeout(() => {
-            deleteText();
-        }, 1000);
-    }
-  }, 100);
+            clearInterval(intervalId);
+            setTimeout(() => {
+                deleteText();
+            }, 1000);
+        }
+    }, 100);
 }
 
 function deleteText() {
-  const intervalId = setInterval(() => {
-    title.textContent = title.textContent.slice(0, -1);
-    if (title.textContent.length === 0) {
-      clearInterval(intervalId);
-      setTimeout(() => {
-        type();
-      }, 1000);
-    }
-  }, 75);
+    const intervalId = setInterval(() => {
+        title.textContent = title.textContent.slice(0, -1);
+        if (title.textContent.length === 0) {
+            clearInterval(intervalId);
+            setTimeout(() => {
+                type();
+            }, 1000);
+        }
+    }, 75);
 }
 
 type();
